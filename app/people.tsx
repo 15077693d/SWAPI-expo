a39545bade1f63c12dc4962fcf0065c471ce0043
15077693d <oscaryiu.lapsang@gmail.com>
2025-01-26 12:07:23 +0800

    Add People page with infinite scroll and detailed view

--- app/people.tsx
+++ app/people.tsx
@@ -1,10 +1,64 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+  RefreshControl,
+} from "react-native";
 import React from "react";
-
-export default function People() {
+import { usePeopleInfiniteQuery } from "@/hook/query";
+import { COLORS } from "@/constants/colors";
+import { People } from "@/types/film";
+export default function Page() {
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } =
+    usePeopleInfiniteQuery();
   return (
-    <View>
-      <Text>People</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      keyExtractor={(item) => item.url}
+      data={data?.pages.reduce((acc, page) => {
+        page.results.forEach((people) => {
+          acc.push(people);
+        });
+        return acc;
+      }, [] as People[])}
+      renderItem={({ item }) => {
+        return (
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              padding: 10,
+              paddingHorizontal: 20,
+              backgroundColor: COLORS.inactive,
+              borderRadius: 10,
+              marginBottom: 10,
+            }}
+            key={item.name}
+          >
+            <Text style={styles.text}>{item.name}</Text>
+            <View>
+              <Text style={styles.text}>Height: {item.height}</Text>
+              <Text style={styles.text}>Gender: {item.gender}</Text>
+            </View>
+          </View>
+        );
+      }}
+      onEndReached={() => fetchNextPage()}
+      onEndReachedThreshold={0.3}
+    ></FlatList>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.containerBg,
+    flex: 1,
+    padding: 10,
+  },
+  text: {
+    color: COLORS.text,
+  },
+});
--- hook/query.ts
+++ hook/query.ts
@@ -1,13 +1,14 @@
 import { FAVORITES_KEY } from "@/constants";
-import { FilmsResponse } from "@/types/film";
+import { FilmsResponse, PeopleResponse } from "@/types/film";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const QUERY_KEY = {
   FILMS: () => ["https://swapi.dev/api/films/"],
   FILM: (id: string) => [`https://swapi.dev/api/films/<id>`, id],
   FAVORITES: () => [`AsyncStorage.getItem(${FAVORITES_KEY})`],
+  PEOPLE: () => ["https://swapi.dev/api/people/"],
 };
 export const useFilms = () => {
   const { data, isLoading, error } = useQuery({
@@ -26,6 +27,7 @@ export const useFilm = (id: string) => {
   );
   return { film, isLoading, error };
 };
+
 export const useFavorites = () => {
   return useQuery({
     queryKey: QUERY_KEY.FAVORITES(),
@@ -33,5 +35,19 @@ export const useFavorites = () => {
       JSON.parse((await AsyncStorage.getItem(FAVORITES_KEY)) || "{}") as {
         [key: string]: boolean;
       },
+  });
+};
+
+export const usePeopleInfiniteQuery = () => {
+  return useInfiniteQuery({
+    queryKey: QUERY_KEY.PEOPLE(),
+    queryFn: async ({ pageParam }) => {
+      const url = pageParam.includes("https://swapi.dev/api/people/")
+        ? pageParam
+        : `https://swapi.dev/api/people/?page=${pageParam}`;
+      return (await axios.get<PeopleResponse>(url)).data;
+    },
+    getNextPageParam: (lastPage) => lastPage.next,
+    initialPageParam: "1",
   });
 };
